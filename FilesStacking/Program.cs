639bf537d36ebda6f98643086dcda5cb18ccb4d0
Udit Jain <udjai@microsoft.com>
2020-08-02 18:26:33 +0530

    Add project files.

---
+++ FilesStacking/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace FilesStacking
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
